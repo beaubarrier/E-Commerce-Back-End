64fc3e871a75c57a2cc4ef1b29ae5156fd4ceea5
Beau Barrier <beaubarrier@gmail.com>
2021-06-23 10:40:57 -0700

    Working on defining tables and fields.

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,27 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_name: {
+      field: 'product_name',
+      type: Sequelize.STRING,
+    },
+    price: {
+      field: 'price',
+      type: Sequelize.DECIMAL
+    },
+    stock: {
+      field: 'stock',
+      type: Sequelize.INTEGER
+    },
+    category_id: {
+      field: 'category_id',
+      type: Sequelize.STRING
+    },
   },
   {
     sequelize,

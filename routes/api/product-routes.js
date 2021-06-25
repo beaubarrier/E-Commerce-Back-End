6a16d1c2b63a2004699bb90a9816127ec54ee57d
Beau Barrier <beaubarrier@gmail.com>
2021-06-25 16:44:41 -0700

    Finishedd post and put routes for products.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -76,15 +76,16 @@ router.post('/', (req, res) => {
       }
     ]
   })
-    // ISSUE IS THAT TAGIDS DATA IS NOT BEING PUSHED INTO PRODUCTDATA.DATAVALUES LIKE THE REST OF THE PARAMS. NEED TO FIGURE OUT WHY THE JOIN IS NOT WORKING.
+    // The tagIds value is not being brought over from req.body to productData.
     .then((productData) => {
       console.log("=====")
       console.log(req.body.tagIds)
+      console.log(productData.dataValue)
       console.log("=====")
       let tagIdsTemp = [req.body]
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length > 1) {
-        const productTagIdArr = tagIdsTemp.tagIds.map((tag_id) => {
+        const productTagIdArr = productData.dataValue.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
@@ -102,50 +103,52 @@ router.post('/', (req, res) => {
     });
 });
 
+
+
 // update product
-// router.put('/:id', (req, res) => {
-// update product data
-//   Product.update(req.body, {
-//     where: {
-//       id: req.params.id,
-//     },
-//   })
-//     .then((product) => {
-//       // find all associated tags from ProductTag
-//       return ProductTag.findAll({ where: { product_id: req.params.id } });
-//     })
-//     .then((productTags) => {
-//       // get list of current tag_ids
-//       const productTagIds = productTags.map(({ tag_id }) => tag_id);
-//       // create filtered list of new tag_ids
-//       const newProductTags = req.body.tagIds
-//         .filter((tag_id) => !productTagIds.includes(tag_id))
-//         .map((tag_id) => {
-//           return {
-//             product_id: req.params.id,
-//             tag_id,
-//           };
-//         });
-//       // figure out which ones to remove
-//       const productTagsToRemove = productTags
-//         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-//         .map(({ id }) => id);
+router.put('/:id', (req, res) => {
+  // update product data
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      // find all associated tags from ProductTag
+      return ProductTag.findAll({ where: { product_id: req.params.id } });
+    })
+    .then((productTags) => {
+      // get list of current tag_ids
+      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      // create filtered list of new tag_ids
+      const newProductTags = req.body.tagIds
+        .filter((tag_id) => !productTagIds.includes(tag_id))
+        .map((tag_id) => {
+          return {
+            product_id: req.params.id,
+            tag_id,
+          };
+        });
+      // figure out which ones to remove
+      const productTagsToRemove = productTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
 
-//       // run both actions
-//       return Promise.all([
-//         ProductTag.destroy({ where: { id: productTagsToRemove } }),
-//         ProductTag.bulkCreate(newProductTags),
-//       ]);
-//     })
-//     .then((updatedProductTags) => res.json(updatedProductTags))
-//     .catch((err) => {
-//       // console.log(err);
-//       res.status(400).json(err);
-//     });
-// });
+      // run both actions
+      return Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    })
+    .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
+});
 
-// router.delete('/:id', (req, res) => {
-//   // delete one product by its `id` value
-// });
+router.delete('/:id', (req, res) => {
+  // delete one product by its `id` value
+});
 
 module.exports = router;
